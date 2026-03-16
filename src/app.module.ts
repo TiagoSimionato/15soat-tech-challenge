48f3c5d06f751723440347307a9ac54574d52f69
Tiago Simionato <tiago.simionato5@gmail.com>
2026-03-16 18:17:47 -0300

    feat: load .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { dbProviders } from './providers/database';
 
 @Module({
   controllers: [],
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
   providers: [...dbProviders],
 })
 export class AppModule {}
