2f388b328707f9fe9ec01564cc5f33985997775d
Tiago <tiago.simionato5@gmail.com>
2026-03-16 19:12:36 -0300

    fix: db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { dbProviders } from './providers/database';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/test/user.entity';
 
 @Module({
   controllers: [],
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
-  providers: [...dbProviders],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_NAME,
+      entities: [User],
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT ?? 5432),
+      synchronize: true,
+      type: 'postgres',
+      username: process.env.DB_USER,
+    }),
+  ],
+  providers: [],
 })
 export class AppModule {}
