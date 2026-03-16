0af1986fad08da63060d1080f17e7d565517d7b9
Tiago Simionato <57016670+TiagoSimionato@users.noreply.github.com>
2026-03-16 19:22:16 -0300

    feat: db connection and dockerfile (#2)
    * feat: type orm
    
    * feat: dockerfile and compose
    
    * feat: db connection setup
    
    * chore: compose cmd readme
    
    * feat: nest config
    
    * feat: load .env
    
    * feat: nestjs typeorm package
    
    * fix: db config
    
    * fix: docker configs
    
    * chore: migration cmd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [],
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_NAME,
+      entities: [],
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT ?? 5432),
+      synchronize: true,
+      type: 'postgres',
+      username: process.env.DB_USER,
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
