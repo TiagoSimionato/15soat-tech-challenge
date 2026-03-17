695d15eb579ee6ad1a0dba0b44abf76f58883431
Tiago <tiago.simionato5@gmail.com>
2026-03-17 12:57:56 -0300

    fix: turn off lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,10 @@ import { lintConfig } from 'tsm-utils/lint';
 
 export default [
   ...lintConfig,
+  {
+    rules: {
+      'consistent-type-imports': 'off',
+      'ts/consistent-type-imports': 'off',
+    },
+  },
 ];
