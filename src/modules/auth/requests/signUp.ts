8fbbe08745bf6f4b1f5822b2cba4b5fe0cb2843a
Tiago <tiago.simionato5@gmail.com>
2026-03-19 15:01:58 -0300

    feat: updated sign up dto

--- src/modules/auth/requests/signUp.ts
+++ src/modules/auth/requests/signUp.ts
@@ -1,12 +1,23 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { LegalNature } from 'src/modules/users/enums/legalNature';
 
 export class SignUpRequest {
   @IsNotEmpty()
+  @IsString()
   username: string;
 
   @IsNotEmpty()
+  @IsString()
   password: string;
 
   @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  document: string;
+
+  @IsEnum(LegalNature, { message: 'legalNature must be [PF] or [PJ]' })
+  legalNature: LegalNature;
 }
