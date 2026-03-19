1b9c2fa6a8ff748222cdc1d42266f07ec72037a5
Tiago <tiago.simionato5@gmail.com>
2026-03-19 16:59:31 -0300

    feat: user crud

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, NotFoundException, Param, Put } from '@nestjs/common';
+import { SignUpRequest } from '../auth/requests/signUp';
 import { UserService } from './users.service';
 
 @Controller({ path: 'users' })
@@ -7,4 +8,34 @@ export class UsersController {
     @Inject()
     private userService: UserService,
   ) {}
+
+  @Get('/list')
+  async listAllUsers() {
+    return await this.userService.listUsers();
+  }
+
+  @Get('/list/:id')
+  async listOneUser(@Param() { id }) {
+    const user = await this.userService.listOneUser(id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  @Put('/update/:id')
+  async updateUser(@Param() { id }, @Body() request: SignUpRequest) {
+    const updateResult = await this.userService.updateUser(id, request);
+    if (updateResult.affected === 0) {
+      throw new NotFoundException();
+    }
+  }
+
+  @Delete('/remove/:id')
+  async deleteUser(@Param() { id }) {
+    const deleteResult = await this.userService.deleteUser(id);
+    if (deleteResult.affected === 0) {
+      throw new NotFoundException();
+    }
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import type { Repository } from 'typeorm';
+import type { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
@@ -6,6 +6,7 @@ import { isCNPJ, isCPF } from 'brazilian-values';
 import { SignUpRequest } from '../auth/requests/signUp';
 import { User } from './entities/users.entity';
 import { LegalNature } from './enums/legalNature';
+import { UserResponseDTO } from './models/UserResponseDTO';
 
 @Injectable()
 export class UserService {
@@ -14,8 +15,8 @@ export class UserService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async findOne(username: string) {
-    return this.usersRepository.findOneBy({ username });
+  async findOne(username: string): Promise<null | User> {
+    return await this.usersRepository.findOneBy({ username });
   }
 
   async create(signUpRequest: SignUpRequest) {
@@ -26,12 +27,38 @@ export class UserService {
       throw new BadRequestException('document is not a valid CNPJ');
     }
 
-    const newUser = new User();
-    newUser.name = signUpRequest.name;
-    newUser.username = signUpRequest.username;
-    newUser.password = await bcrypt.hash(signUpRequest.password, 10);
-    newUser.document = signUpRequest.document;
-    newUser.legalNature = signUpRequest.legalNature;
+    const newUser = this.usersRepository.create({
+      document: signUpRequest.document,
+      legalNature: signUpRequest.legalNature,
+      name: signUpRequest.name,
+      password: await this.encryptPassword(signUpRequest.password),
+      username: signUpRequest.username,
+    });
     await this.usersRepository.save(newUser);
+  }
+
+  async listUsers(): Promise<UserResponseDTO[]> {
+    return (await this.usersRepository.find()).map(user => new UserResponseDTO({ ...user }));
+  }
+
+  async listOneUser(id: number): Promise<null | UserResponseDTO> {
+    const userEntity = await this.usersRepository.findOneBy({ id });
+    if (!userEntity)
+      return null;
+
+    return new UserResponseDTO({ ...userEntity });
+  }
+
+  async updateUser(id: number, user: SignUpRequest): Promise<UpdateResult> {
+    user.password = await this.encryptPassword(user.password);
+    return await this.usersRepository.update({ id }, user);
+  }
+
+  async deleteUser(id: number): Promise<DeleteResult> {
+    return await this.usersRepository.delete({ id });
+  }
+
+  private async encryptPassword(password: string) {
+    return await bcrypt.hash(password, 10);
   }
 }
