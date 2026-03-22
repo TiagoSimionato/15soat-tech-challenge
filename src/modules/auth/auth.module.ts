8dfdd8ca70b8297082a9f7f022ca055aa085ae91
Tiago <tiago.simionato5@gmail.com>
2026-03-22 14:37:29 -0300

    feat: load guards

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
-import { AuthGuard } from './auth.guard';
 import { jwtConstants } from './constants/auth.constants';
 import { AuthController } from './controllers/auth.controller';
+import { AuthGuard } from './guards/auth.guard';
+import { RolesGuard } from './guards/roles.guards';
 import { AuthService } from './services/auth.service';
 
 @Module({
@@ -21,6 +22,10 @@ import { AuthService } from './services/auth.service';
     {
       provide: 'APP_GUARD',
       useClass: AuthGuard,
+    },
+    {
+      provide: 'APP_GUARD',
+      useClass: RolesGuard,
     },
   ],
 })
