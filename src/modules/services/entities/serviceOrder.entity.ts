69247bc65a87cbd58f8787f39be9bd3dbca6c172
Tiago <tiago.simionato5@gmail.com>
2026-04-26 14:56:48 -0300

    feat: service order vehicle dates

--- src/modules/services/entities/serviceOrder.entity.ts
+++ src/modules/services/entities/serviceOrder.entity.ts
@@ -19,6 +19,12 @@ export class ServiceOrder {
   @Column({ transformer: new ColumnNumericTransformer(), type: 'numeric' })
   cost: number;
 
+  @Column({ nullable: true })
+  vehicle_arrived_at: Date;
+
+  @Column({ nullable: true })
+  vehicle_delivered_at: Date;
+
   @ManyToOne(() => User, user => user.orders)
   @JoinColumn({ name: 'user_id' })
   user: User;
