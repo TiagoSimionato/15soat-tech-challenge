1177a97f6b7dfaa1d64be4b2ce4cb69d71ee48da
Nathalia Dutra <nathaliad.oliveira@hotmail.com>
2026-06-25 19:00:32 -0300

    add ghcr

--- infra/vars.tf
+++ infra/vars.tf
@@ -10,5 +10,7 @@ variable "postgres_user"     {}
 variable "postgres_password" {}
 variable "postgres_db"       {}
 variable "jwt_secret"        {}
+variable "ghcr_token"        {}
+variable "ghcr_username"     {}
 variable "db_host"           { default = "db-svc-15soat-tech-challenge" }
 variable "db_port"           { default = "5432" }
