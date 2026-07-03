754b312875e6d8fdac1c4446a0c0f28996905675
Nathalia Dutra <nathaliad.oliveira@hotmail.com>
2026-07-03 19:01:14 -0300

    add api deployment on terraform script

--- infra/deployment.tf
+++ infra/deployment.tf
@@ -59,3 +59,120 @@ resource "kubernetes_deployment" "db" {
   }
   depends_on = [kubernetes_namespace.app]
 }
+
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name      = "app-15soat-tech-challenge"
+    namespace = kubernetes_namespace.app.metadata[0].name
+    labels    = { app = "app-15soat-tech-challenge" }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = { app = "app-15soat-tech-challenge" }
+    }
+
+    template {
+      metadata {
+        name   = "app-15soat-tech-challenge"
+        labels = { app = "app-15soat-tech-challenge" }
+      }
+
+      spec {
+        automount_service_account_token = false
+
+        image_pull_secrets {
+          name = "ghcr-secrets"
+        }
+
+        container {
+          name  = "c-15soat-tech-challenge"
+          image = var.image
+
+          resources {
+            requests = {
+              cpu               = "1"
+              memory            = "100Mi"
+              ephemeral-storage = "2Gi"
+            }
+            limits = {
+              cpu    = "2"
+              memory = "100Mi"
+            }
+          }
+
+          port {
+            container_port = 3000
+          }
+
+          env {
+            name = "DB_NAME"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.app-secrets.metadata[0].name
+                key  = "POSTGRES_DB"
+              }
+            }
+          }
+
+          env {
+            name = "DB_USER"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.app-secrets.metadata[0].name
+                key  = "POSTGRES_USER"
+              }
+            }
+          }
+
+          env {
+            name = "DB_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.app-secrets.metadata[0].name
+                key  = "POSTGRES_PASSWORD"
+              }
+            }
+          }
+
+          env {
+            name = "DB_HOST"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.app-secrets.metadata[0].name
+                key  = "DB_HOST"
+              }
+            }
+          }
+
+          env {
+            name = "DB_PORT"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.app-secrets.metadata[0].name
+                key  = "DB_PORT"
+              }
+            }
+          }
+
+          env {
+            name = "JWT_SECRET"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.app-secrets.metadata[0].name
+                key  = "JWT_SECRET"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    kubernetes_secret.app-secrets,
+    kubernetes_deployment.db
+  ]
+}
