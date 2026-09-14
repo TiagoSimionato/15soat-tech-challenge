70e92e85dc91779b5b294a10d46179700d661b1a
Tiago Simionato <57016670+TiagoSimionato@users.noreply.github.com>
2026-09-14 19:15:51 -0300

    feat: update deploy (#59)

--- infra/namespace.tf
+++
@@ -1,7 +0,0 @@
-resource "kubernetes_namespace" "app" {
-  metadata {
-    name = var.namespace
-  }
-
-  depends_on = [kind_cluster.main]
-}
--- infra/vars.tf
+++ infra/vars.tf
@@ -6,12 +6,16 @@ variable "namespace" {
   default = "soat-grupo76"
 }
 
-variable "image"             {}
-variable "postgres_user"     {}
-variable "postgres_password" {}
-variable "postgres_db"       {}
-variable "jwt_secret"        {}
-variable "ghcr_token"        {}
-variable "ghcr_username"     {}
-variable "db_host"           { default = "db-svc-15soat-tech-challenge" }
-variable "db_port"           { default = "5432" }
+variable "image"                       {}
+variable "postgres_user"               {}
+variable "postgres_password"           {}
+variable "postgres_db"                 {}
+variable "jwt_secret"                  {}
+variable "ghcr_token"                  {}
+variable "ghcr_username"               {}
+variable "db_host"                     { default = "db-svc-15soat-tech-challenge" }
+variable "db_port"                     { default = "5432" }
+variable "kind_endpoint"               {}
+variable "kind_client_certificate"     {}
+variable "kind_client_key"             {}
+variable "kind_cluster_ca_certificate" {}
